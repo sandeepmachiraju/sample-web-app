569523f53d3eb84a29762742819211102453ab34
sandeepmachiraju <mssaisandeep@gmail.com>
2024-07-11 20:22:29 +0000

    Fixed few issues

--- main.js
+++ main.js
@@ -17,10 +17,21 @@ require('dotenv').config();
 app.use(helmet());
 
 // Middleware to enforce HSTS for a specified duration
+// 'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload'
 app.use(helmet.hsts({
   maxAge: 31536000,     // 1 year in seconds
   includeSubDomains: true,
   preload: true
+}));
+
+// Middleware to allow unsafe scripts to run
+// 'Content-Security-Policy': "script-src 'self' 'unsafe-inline'"
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'", "'unsafe-inline'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    connectSrc: ["*"],
+  }
 }));
 
 // Serve static files from the "public" directory
@@ -38,38 +49,36 @@ app.use((req, res, next) => {
 // Define the hello API route with strict HSTS
 app.get('/api/hello', (req, res) => {
   // Set a cookie example
-  res.cookie('myCookie', 'someValue', { httpOnly: true, secure: true });
-  res.status(200).json({ message: 'Hello Node!' });
+  res.cookie('myCookie', 'someValue', { httpOnly: false, secure: true, sameSite: 'Strict', path: '/' });
+  res.status(200).json({ message: 'Hello! How are you doing today? :)' });
 });
 
-// Define the todos API route
-app.get('/api/todos', async (req, res) => {
+// Define the without-cross-origin-haders API route
+app.get('/api/without-cross-origin-null-headers', async (req, res) => {
   try {
-    const apiResponse = { "userId": 1, "id": 1, "title": "foobar" }
+    const apiResponse = { "userId": 1, "id": 1, "name": "Number 1" }
     res.status(200).json(apiResponse);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Inject environment variable into HTML and serve it
+// Define the api-with-cross-origin-headers API route
+app.get('/api/with-cross-origin-null-headers', async (req, res) => {
+  try {
+    const apiResponse = { "userId": 1, "id": 1, "name": "Number 1" }
+    res.setHeader('Access-Control-Allow-Origin', 'null');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.status(200).json(apiResponse);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Serving the index.html
 app.get('/', (req, res) => {
   const indexPath = path.join(__dirname, 'public', 'index.html');
-  // Add HTTP Strict Transport Security (HSTS) header
-  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
-  res.setHeader('Content-Security-Policy', 'script-src \'self\' \'unsafe-inline\'');
-  
-  fs.readFile(indexPath, 'utf8', (err, html) => {
-    if (err) {
-      res.status(500).send('Error reading index.html');
-      return;
-    }
-    // Replace the placeholder with the actual API hostname
-    const modifiedHtml = html.replace('__API_HOSTNAME__', process.env.API_HOSTNAME);
-    console.log('Modified index.html content:');
-    console.log(modifiedHtml);
-    res.send(modifiedHtml);
-  });
+  res.sendFile(indexPath);
 });
 
 // Start server
--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
 docker run -p 0.0.0.0:3000:3000 -d --name sample-web-app my-node-app
-
